5d819f1a2072ccc41559e5663723d552f1ef0d0b
Nate Wessel <bike756@gmail.com>
2020-07-24 12:56:42 -0400

    refactor

--- bundle.js
+++ bundle.js
@@ -1082,8 +1082,8 @@ function select(selector) {
       : new Selection([[selector]], root);
 }
 
-var USD = new Intl.NumberFormat('en-CA',{style:'currency',currency:'USD'});
-var NUM = new Intl.NumberFormat();
+const USD = new Intl.NumberFormat('en-CA',{style:'currency',currency:'USD'});
+const NUM = new Intl.NumberFormat();
 
 const provinces = [
 	{abbr:'BC',full:'British Columbia'},
@@ -1117,11 +1117,18 @@ csv$1('./data/HScodes.csv').then( HScodes => {
 
 // if previosuly submitted, show us some data!
 var params = new URLSearchParams( window.location.search );
-if ( params.get('hs6') ) {
-	let HSval = params.get('hs6');
+if ( params.get('hs6') && /^\d{6}$/.test(params.get('hs6')) ) {
+	const HSval = params.get('hs6');
+	const infoBox = select('#category-info');
+	const A = infoBox.append('div');
+	const B = infoBox.append('div');
+	addOurData(HSval,A);
+	addComtradeData(HSval,B);
+}
+
+function addOurData(hscode,container){
 	csv$1('./data/the-data.csv').then( tariffData => {
-		let record = tariffData.find( r => r.HS6 == `'${HSval}` );
-		let infoBox = select('#category-info');
+		let record = tariffData.find( r => r.HS6 == `'${hscode}` );
 		// variable name mapping, etc
 		let description = record['Description'];
 		let tariffRate = record['Japan Rate for Canada TPP'];
@@ -1135,71 +1142,66 @@ if ( params.get('hs6') ) {
 			return dollars >= 500 ? text : null
 		}).filter( val => val );
 		// append data to DOM
-		infoBox.append('h2').text( `${HSval} - ${description}` );
-		infoBox.append('h3').text('Tariff Rate');
-		infoBox.append('p').text(tariffRate);
-		infoBox.append('h3').text('Market Opportunity');
-		infoBox.append('p')
+		container.append('h2').text( `${hscode} - ${description}` );
+		container.append('h3').text('Tariff Rate');
+		container.append('p').text(tariffRate);
+		container.append('h3').text('Market Opportunity');
+		container.append('p')
 			.text(`${USD.format(canadaGain)} (+${canadaGainPercent}%)`);
 		if( provincialGains.length > 0 ){
-			infoBox.append('h3').text('Provincial Gains');
+			container.append('h3').text('Provincial Gains');
 			provincialGains.forEach( content => {
-				infoBox.append('p').text(content);
+				container.append('p').text(content);
 			});
 		}
-		// get external top-5 data
-		infoBox.append('h3').text('Top Global Exporters to Japan');
-		infoBox.append('p').attr('id','loading').text('loading...');
-		// https://comtrade.un.org/Data/Doc/API
-		let params = new URLSearchParams({
-			'r':392,    // data reported by/for Japan
-			'p':'all',  // for all partner regions
-			'freq':'A', // annual data
-			'ps':'now', // latest time period
-			'px':'HS',  // search by HS code
-			'cc':HSval, // selected HS code
-			'rg':1,     // imports only
-			'max':500   // max records returned
-		});
-		let url = `https://comtrade.un.org/api/get?${params}`;
-		json(url).then( response => {
-			let data = response.dataset.sort((a,b)=>b.TradeValue-a.TradeValue);
-			let world = data.shift();
-			// get Canada's position
-			let canIndex = data.findIndex( d => d.ptTitle == 'Canada' );
-			let topN = data.slice(0, canIndex+1 > 5 ? canIndex+1 : 5 );
-			infoBox.select('p#loading').remove();
-			// create a table for results
-			const columns = [
-				{ 
-					apiKey:'ptTitle', 
-					label:'Country', 
-					format: text=>text
-				},
-				{ 
-					apiKey:'TradeValue',
-					label:'Value of Exports (USD)',
-					format: NUM.format
-				}
-			];
-			let table = infoBox.append('table');
-			table
-				.append('thead')
-				.append('tr')
-				.selectAll('th')
-				.data(columns)
-				.join('th')
-				.text(d=>d.label);
-			table
-				.append('tbody')
-				.selectAll('tr')
-				.data(topN)
-				.join('tr')
-				.selectAll('td')
-				.data( d => columns.map( c => c.format( d[c.apiKey] ) ) ) 
-				.join('td')
-				.style('font-weight',text=>text=='Canada'?'bold':null)
-				.text(text=>text);
-		});
+	});	
+}
+
+function addComtradeData(hscode,container){
+	// get external top-5 data
+	container.append('h3').text('Top Global Exporters to Japan');
+	container.append('p').attr('id','loading').text('loading...');
+	// https://comtrade.un.org/Data/Doc/API
+	let params = new URLSearchParams({
+		'r':392,    // data reported by/for Japan
+		'p':'all',  // for all partner regions
+		'freq':'A', // annual data
+		'ps':'now', // latest time period
+		'px':'HS',  // search by HS code
+		'cc':hscode, // selected HS code
+		'rg':1,     // imports only
+		'max':500   // max records returned
+	});		
+	let url = `https://comtrade.un.org/api/get?${params}`;
+	const columns = [
+		{ apiKey:'ptTitle', label:'Country', format: text=>text },
+		{ apiKey:'TradeValue', label:'Value of Exports (USD)', format: NUM.format }
+	];
+	json(url).then( response => {
+		let data = response.dataset.sort((a,b)=>b.TradeValue-a.TradeValue);
+		let world = data.shift(); // world should always be the largest
+		// get Canada's position
+		let canIndex = data.findIndex( d => d.ptTitle == 'Canada' );
+		let topN = data.slice(0, canIndex+1 > 5 ? canIndex+1 : 5 );
+		container.select('p#loading').remove();
+		// create a table for results
+		let table = container.append('table');
+		table
+			.append('thead')
+			.append('tr')
+			.selectAll('th')
+			.data(columns)
+			.join('th')
+			.text(d=>d.label);
+		table
+			.append('tbody')
+			.selectAll('tr')
+			.data(topN)
+			.join('tr')
+			.selectAll('td')
+			.data( d => columns.map( c => c.format( d[c.apiKey] ) ) ) 
+			.join('td')
+			.style('font-weight',text=>text=='Canada'?'bold':null)
+			.text(text=>text);		
 	});
 }
