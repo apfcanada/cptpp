9abbeabfdb3752a6d0b445adafa44488cc191894
Nate Wessel <bike756@gmail.com>
2020-07-30 10:36:12 -0400

    stop reloading page

--- main.js
+++ main.js
@@ -22,17 +22,6 @@ const regions = [
 	{abbr:'MB',full:'Manitoba'}
 ]
 
-// if previously submitted, render data
-if (HScode) { 
-	const infoBox = select('#category-info')
-	const title = infoBox.append('h2').text(HScode)
-	const desc = infoBox.append('p').attr('id','HSdescription')
-	const Adiv = infoBox.append('div')
-	const Bdiv = infoBox.append('div')
-	addOurData(HScode,Adiv)
-	addComtradeData(HScode,Bdiv)
-}
-
 // create the search box, populated with data
 csv('./data/unified-data.csv').then( HScodes => {
 	// enter the description for the selected code
@@ -54,7 +43,8 @@ csv('./data/unified-data.csv').then( HScodes => {
 				if(d.hasEstimatedGain){ return text }
 				return `<span style="color:gray;">${text}</span>`
 			}
-		}
+		},
+		onConfirm: updatePage
 	})
 	function suggest (query, syncResults) {
 		query = query.toLowerCase()
@@ -70,68 +60,47 @@ csv('./data/unified-data.csv').then( HScodes => {
 	}
 })
 
-function addOurData(hscode,container){
-	csv('./data/unified-data.csv').then( response => {
-		const record = response.find( r => r.HScode == hscode )
-		console.log(record)
-		if ( record.hasEstimatedGain != 'TRUE' ) { 
-			container.append('p')
-				.text('Not affected by the CPTPP. Find a tariff at ')
-				.append('a').attr('href','https://www.tariffinder.ca')
-				.text('tariffinder.ca')
-			return 
+function updatePage(data){
+	if(!data) return
+	console.log(data)
+	addComtradeData(data)
+	select('#infoBox').style('display','block')
+	select('h2#HS').text(data.HScode)
+	select('p#HSdescription').text(data.description)
+	select('#oldTariffRate').text( TRF.format(data.initialTariffRate))
+	select('#newTariffRate').text( TRF.format(data.tariffRate) )
+	
+	if ( data.hasEstimatedGain == 'FALSE' ) { 
+		return select('p#noEffect')
+			.text('Not affected by the CPTPP. Find a tariff at ')
+			.append('a').attr('href','https://www.tariffinder.ca')
+			.text('tariffinder.ca')
+	}
+	// get region data
+	const gains = regions.map( prov => {
+		return {
+			'name': prov.full,
+			'gain': data[`${prov.abbr}gain`],
+			'change': data[`${prov.abbr}gainPercent`]
 		}
-		// get region data
-		const gains = regions.map( prov => {
-			return {
-				'name': prov.full,
-				'gain': record[`${prov.abbr}gain`],
-				'change': record[`${prov.abbr}gainPercent`]
-			}
-		}).sort((a,b)=>b.gain-a.gain)
-		// append data to DOM
-		container.append('h3').text('Tariff Rate')
-		let para = container.append('p')
-		para.append('del')
-			.attr('id','oldTariffRate')
-			.attr('title','Tariff rate prior to CPTPP')
-			.text(TRF.format(record.initialTariffRate))
-		para.append('span')
-			.attr('id','newTariffRate')
-			.attr('title','Current tariff rate under CPTPP')
-			.text(TRF.format(record.tariffRate))
-		container
-			.append('h3')
-			.text('Expected Gain for Canada and the Western Provinces')
-		const table = container.append('table')
-		table
-			.append('thead')
-			.append('tr')
-			.selectAll('th')
-			.data(['','Gain','Change from baseline'])
-			.join('th')
-			.text(d=>d)
-		const rows = table
-			.append('tbody')
-			.selectAll('tr')
-			.data(gains)
-			.join('tr')
-			.style('font-weight', p => p.name == 'Canada' ? 'bold' : null )
-		rows.append('td').text( p => p.name )
-		rows.append('td')
-			.text( p => p.gain > 0 ? USD.format(p.gain) : 'None')
-			.style('text-align','right')
-		rows.append('td')
-			.text( p => p.gain > 0 ? PCT.format(p.change): 'NA' )
-	})	
+	}).sort((a,b)=>b.gain-a.gain)
+	select('table#regionalGains tbody')
+		.selectAll('tr')
+		.data(gains)//.id(d=>d.abbr)
+		.join('tr')
+		.style('font-weight', d => d.name == 'Canada' ? 'bold' : null )
+		.selectAll('td')
+		.data(d => [ 
+			d.name,  
+			d.gain > 0 ? USD.format(d.gain) : 'None',
+			d.gain > 0 ? PCT.format(d.change): 'NA'
+		] )
+		.join('td')
+		.text(t=>t)
 }
 
-function addComtradeData(hscode,container){
-	// get external top-5 data
-	container.append('h3')
-		.text('Top Global Exporters to Japan ')
-		.append('span').attr('id','latestYear')
-	container.append('p').attr('id','loading').text('loading...')
+function addComtradeData(data){
+	let container = select('#comtradeData')
 	// https://comtrade.un.org/Data/Doc/API
 	let params = new URLSearchParams({
 		'r':392,    // data reported by/for Japan
@@ -139,7 +108,7 @@ function addComtradeData(hscode,container){
 		'freq':'A', // annual data
 		'ps':'now', // latest time period
 		'px':'HS',  // search by HS code
-		'cc':hscode, // selected HS code
+		'cc':data.HScode, // selected HS code
 		'rg':1,     // imports only
 		'max':500   // max records returned
 	})		
@@ -156,24 +125,14 @@ function addComtradeData(hscode,container){
 		const topN = data.slice(0, canIndex+1 >= 5 ? canIndex+1 : 5 )
 		container.select('p#loading').remove()
 		// create a table for results
-		const table = container.append('table')
-		table
-			.append('thead')
-			.append('tr')
-			.selectAll('th')
-			.data(['Country','Rank','Value of Exports'])
-			.join('th')
-			.text(t=>t)
-		const rows = table.append('tbody')
+		container.select('table tbody')
 			.selectAll('tr')
 			.data(topN)
 			.join('tr')
 			.style('display',d=>d.rank<=4||d.ptTitle=='Canada'?null:'none')
 			.style('font-weight',d=>d.ptTitle=='Canada'?'bold':null)
-		rows.append('td').text(d=>d.ptTitle)
-		rows.append('td').text( d => d.rank )
-		rows.append('td')
-			.text( d => USD.format(d.TradeValue) )
-			.style('text-align','right')
+			.selectAll('td')
+			.data( d=> [ d.ptTitle, d.rank, USD.format(d.TradeValue) ] )
+			.join('td').text(t=>t)
 	})
 }
